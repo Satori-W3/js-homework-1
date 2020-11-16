63f4de16fa67c79d49c6786a01337c1a71a7ce3a
Satori <40677414+Satori-W3@users.noreply.github.com>
2020-11-16 17:08:18 +0400

    first

---
+++ assets/script/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+//homework (1) / factorialize
+
+// let number = +document.getElementsByClassName("factorial")
+
+// function factorialize(numb){
+//     if (numb === 0 || numb === 1)
+//         return 1;
+//     // else if (numb < 0)
+//     //     return Math.abs(number);
+//     for(let x = numb - 1; x >= 1; x--){
+//         numb *= x; 
+//     }
+//         return  numb;
+// };
+
+// function factorializeResult(){
+//     document.getElementById("result")console.log(factorialize(number));
+// };
+
+// let a = +prompt('ჩაწერე პირველი ციფრი')
+// let b = +prompt('ჩაწერე მეორე ციფრი')
+
+//homework (2) / math.max
+
+// function myFunction() {
+//     return(Math.max(a, b));
+// }
+
+// console.log(myFunction(a, b));
+
+//homework (3) / calculator
+
+// let a = +prompt('ჩაწერეთ პირველი რიცხვი');
+// let b = prompt('შეიყვანეთ მოქმედება: "+", "-", "*", "/".');
+// let c = +prompt('ჩაწერეთ მეორე რიცხვი');
+
+// function calculator() {
+//     if(b == "+")
+//         return(a + c);
+//     else if(b == "-"){
+//         return(a - c);
+//     }
+//     else if(b == "*"){
+//         return(a * c);
+//     }
+//     else if(b == "/"){
+//         return(a / c);
+//     }
+//     else('თქვენ არღვევთ წესებს!!!')
+// }
+
+// console.log(calculator(a, b, c));
+
+//homework (4) / power
+
+// let a = +prompt('რიცხვი')
+// let b = +prompt('შეიყვანეთ ხარისხის რაოდენობა')
+
+// function myFunction() {
+//     return(Math.pow(a, b))
+// }
+
+// console.log(myFunction(a, b))
+
+//homework (5) age
+
+// let day = +prompt('თქვენი დაბადების რიცხვი')
+// let month = +prompt('თქვენი დაბადების თვე')
+// let year = +prompt('თქვენი დაბადების წელი')
+
+// function myFunction() {
+//     return(2020 - year);
+// }
+
+// console.log(myFunction(day, month, year))
+
+
